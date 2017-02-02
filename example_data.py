f0740e239ada989ce8d8399be84da7e4da472a47
keligeri <kel.gergo@gmail.com>
2017-02-02 16:59:10 +0100

    REpo cleaning

--- example_data.py
+++
@@ -1,8 +0,0 @@
-# This script can generate example data for "City" and "InterviewSlot" models.
-
-from models import *
-from datetime import datetime
-
-
-#class GenerateExampleData:
-#    def __init__(self):

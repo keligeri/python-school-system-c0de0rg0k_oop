152d6b0ca4ad222ff6b63c036e5355b9b72d15ae
keligeri <kel.gergo@gmail.com>
2017-02-02 17:02:03 +0100

    Repo cleaning

--- proba.py
+++
@@ -1,7 +0,0 @@
-from build import BuildTable
-from example_data import GenerateExampleData
-from new_applicants import GenerateApplicants
-
-BuildTable()
-GenerateExampleData()
-GenerateApplicants()
